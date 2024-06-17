b04013652509e4ce6bf9dd4cc4ac9a2e3aef7634
hyejin <hyejda00@naver.com>
2024-06-18 03:54:48 +0900

    Feat: User CRUD 코드 작성

---
+++ src/main/java/sparta/streaming/user/UserRepository.java
@@ -0,0 +1,9 @@
+package sparta.streaming.user;
+
+import sparta.streaming.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
