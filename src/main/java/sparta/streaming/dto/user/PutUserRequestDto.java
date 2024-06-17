cac9afaddd69e93fbef38587fa798394f1da76cd
hyejin <hyejda00@naver.com>
2024-06-18 04:04:28 +0900

    Refactor: Dto 생성

---
+++ src/main/java/sparta/streaming/dto/user/PutUserRequestDto.java
@@ -0,0 +1,14 @@
+package sparta.streaming.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class PutUserRequestDto extends UserCommonDto {
+    private Long userId;
+}
