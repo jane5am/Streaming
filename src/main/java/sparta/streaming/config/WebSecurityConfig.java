96e3d7ce3306db345018f8ec052d581ea74a8a5b
hyejin <hyejda00@naver.com>
2024-06-20 15:55:55 +0900

    Feat: jwt - WebSecurityConfig 추가

---
+++ src/main/java/sparta/streaming/config/WebSecurityConfig.java
@@ -0,0 +1,86 @@
+package sparta.streaming.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import sparta.streaming.filter.JwtAuthenticationFilter;
+
+import java.io.IOException;
+
+@Configurable
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.cors(cors -> cors
+                .configurationSource(corsConfigurationSource())
+        )
+        .csrf(CsrfConfigurer::disable)
+        .httpBasic(HttpBasicConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)//세션유지하지않겠다
+        )
+                .authorizeHttpRequests(request -> request
+                .requestMatchers("/","/api/v1/auth/**").permitAll()//이 패턴에 대해서는 모두 허용하겠다
+                .requestMatchers("/api/v1/user/**").hasRole("USER")
+                .requestMatchers("/api/v1/seller/**").hasRole("SELLER")
+                                .anyRequest().authenticated()
+
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(new FailedAuthenticationEntryPoint())
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 모든 출처에 대해 모두 허용
+        corsConfiguration.addAllowedMethod("*"); // 모든 메소드에 대해 허용
+        corsConfiguration.addAllowedHeader("*"); // 모든 헤더에 대해 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/v1/**", corsConfiguration);
+
+        return source;
+    }
+
+}
+
+
+// 인가실패 인 경우
+class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        //{"code":"NP","message" : "No permission:}
+        response.getWriter().write("{\"code\":\"NP\",\"message\" : \"No permission:}");
+    }
+}
